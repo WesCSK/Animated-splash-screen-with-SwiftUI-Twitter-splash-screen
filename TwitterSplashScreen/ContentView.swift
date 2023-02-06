3ec5209199c544a791cfa263ecfe0e2f6984252f
Swee Kwang <sweekwang94@gmail.com>
2023-02-06 21:51:17 +0800

    Files removed for starting project

--- TwitterSplashScreen/ContentView.swift
+++ TwitterSplashScreen/ContentView.swift
@@ -12,37 +12,7 @@ struct ContentView: View {
     @State var scaleAmount: CGFloat = 1
     
     var body: some View {
-        ZStack {
-            Color("twitter_blue")
-            
-            if isHomeRootScreen {
-                HomeScreen()
-            } else {
-                Image("twitter_logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .scaleEffect(scaleAmount)
-                    .frame(width: 80)
-            }
-        }
-        .ignoresSafeArea()
-        .onAppear {
-            // Shrink the icon
-            withAnimation(.easeOut(duration: 0.5)) {
-                scaleAmount = 0.6
-            }
-            
-            // Enlarge the logo
-            withAnimation(.easeInOut(duration: 1).delay(0.5)) {
-                scaleAmount = 80
-
-            }
-            
-            // Do somehing after the animation is done
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5){
-                isHomeRootScreen = true
-            }
-        }
+        HomeScreen()
     }
 }
 
